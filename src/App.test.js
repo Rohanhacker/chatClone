8034944e5c977536737ceceff15ce6018fd4d144
Rohan Malhotra <rohan.root@gmail.com>
2018-12-10 10:46:37 +0530

    set up enzyme and added test for handleMessage

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import { shallow } from "enzyme"
+import App from "./App"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+it("renders without crashing", () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it("handleMessage adds new message to state", () => {
+  const wrapper = shallow(<App />)
+  const instance = wrapper.instance()
+  const mockMsg = {
+    msg: "hello",
+    to: "9192121222",
+    from: "me",
+  }
+  instance.handleMessage({ data: JSON.stringify(mockMsg) })
+  expect(instance.state.allMessages).toEqual({
+    "9192121222": [
+      {
+        msg: "hello",
+        from: "9192121222",
+        to: "me",
+      },
+    ],
+  })
+})
